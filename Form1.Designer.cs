4e81d3d5874fe77a5d44b73e7113ef61b91abbec
Zanvis <legoo1312@gmail.com>
2024-05-24 18:59:25 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace AnimationPL2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnLosuj = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(135, 116);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1248, 733);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.Location = new System.Drawing.Point(416, 25);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(64, 64);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            this.btnStart.MouseHover += new System.EventHandler(this.btnStart_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnStop.Image = ((System.Drawing.Image)(resources.GetObject("btnStop.Image")));
+            this.btnStop.Location = new System.Drawing.Point(346, 25);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(64, 64);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            this.btnStop.MouseHover += new System.EventHandler(this.btnStop_MouseHover);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnReset.Image = ((System.Drawing.Image)(resources.GetObject("btnReset.Image")));
+            this.btnReset.Location = new System.Drawing.Point(276, 25);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(64, 64);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            this.btnReset.MouseHover += new System.EventHandler(this.btnReset_MouseHover);
+            // 
+            // btnLosuj
+            // 
+            this.btnLosuj.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnLosuj.Image = ((System.Drawing.Image)(resources.GetObject("btnLosuj.Image")));
+            this.btnLosuj.Location = new System.Drawing.Point(206, 25);
+            this.btnLosuj.Name = "btnLosuj";
+            this.btnLosuj.Size = new System.Drawing.Size(64, 64);
+            this.btnLosuj.TabIndex = 4;
+            this.btnLosuj.UseVisualStyleBackColor = true;
+            this.btnLosuj.Click += new System.EventHandler(this.btnLosuj_Click);
+            this.btnLosuj.MouseHover += new System.EventHandler(this.btnLosuj_MouseHover);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(500, 65);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(243, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(547, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 36);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Balls speed";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(1003, 51);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(59, 39);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            12,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(780, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(227, 36);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Number of balls:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1522, 874);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.btnLosuj);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Antoni Piwowarski 62024";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnLosuj;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimationPL2
+{
+    public partial class Form1 : Form
+    {
+        private List<Ball> apballs = new List<Ball>();
+        private Random aprandom = new Random();
+        int apinterval = 5, apnumber = 12;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = apinterval;
+            for (int i = 0; i < apnumber; i++)
+            {
+                apballs.Add(new Ball(pictureBox1, aprandom));
+            }
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = apinterval;
+            timer1.Start();
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            CheckAndResolveCollisions();
+            foreach (var apball in apballs)
+            {
+                apball.Move();
+                apball.UpdateParticles();
+            }
+
+            pictureBox1.Invalidate();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var apball in apballs)
+            {
+                apball.Draw(e.Graphics);
+                apball.DrawParticles(e.Graphics);
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            foreach (var apball in apballs)
+            {
+                apball.Reset();
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void btnLosuj_Click(object sender, EventArgs e)
+        {
+            int apcurrentCount = apballs.Count;
+            int apdesiredCount = apnumber;
+
+            if (apcurrentCount < apdesiredCount)
+            {
+                for (int i = apcurrentCount; i < apdesiredCount; i++)
+                {
+                    apballs.Add(new Ball(pictureBox1, aprandom));
+                }
+            }
+            else if (apcurrentCount > apdesiredCount)
+            {
+                apballs = apballs.Take(apdesiredCount).ToList();
+            }
+            foreach (var apball in apballs)
+            {
+                apball.RandomizePosition();
+            }
+            pictureBox1.Invalidate();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar1.Value > 5)
+                apinterval = trackBar1.Value * 3;
+            else if (trackBar1.Value <= 5)
+                apinterval = trackBar1.Value;
+            if (apinterval < 1)
+                apinterval = 1;
+            timer1.Interval = apinterval;
+        }
+
+        private void btnStart_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Play the animation", btnStart);
+        }
+
+        private void btnStop_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Stop the animation", btnStop);
+        }
+
+        private void btnReset_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Move back to the initial position of the balls", btnReset);
+        }
+
+        private void btnLosuj_MouseHover(object sender, EventArgs e)
+        {
+            toolTip1.Show("Draw new ball positions", btnLosuj);
+        }
+        private void CheckAndResolveCollisions()
+        {
+            for (int i = 0; i < apballs.Count; i++)
+            {
+                for (int j = i + 1; j < apballs.Count; j++)
+                {
+                    Ball apball1 = apballs[i];
+                    Ball apball2 = apballs[j];
+                    float apdx = apball2.apX - apball1.apX;
+                    float apdy = apball2.apY - apball1.apY;
+                    float apdistance = (float)Math.Sqrt(apdx * apdx + apdy * apdy);
+                    if (apdistance < (apball1.apSize / 2) + (apball2.apSize / 2))
+                    {
+                        float aptempVx = apball1.apVelocityX;
+                        float aptempVy = apball1.apVelocityY;
+                        apball1.apVelocityX = apball2.apVelocityX;
+                        apball1.apVelocityY = apball2.apVelocityY;
+                        apball2.apVelocityX = aptempVx;
+                        apball2.apVelocityY = aptempVy;
+                        apball1.GenerateExplosion(aprandom);
+                        apball2.GenerateExplosion(aprandom);
+                    }
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            apnumber = (int)numericUpDown1.Value;
+        }
+    }
+    public class Ball
+    {
+        public float apX, apY, apVelocityX, apVelocityY, apSize;
+        public Color apColor;
+        private PictureBox appictureBox;
+        private List<PointF> aptrail = new List<PointF>();
+        private const int apTrailLength = 12;
+        private Random aprandom;
+        private int apinitialX, apinitialY;
+        private float apinitialVelocityX, apinitialVelocityY;
+        private List<Particle> explosionParticles = new List<Particle>();
+
+        public Ball(PictureBox appb, Random aprandom)
+        {
+            appictureBox = appb;
+            this.aprandom = aprandom;
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            int apminEdgeDistance = 10;
+            apSize = aprandom.Next(12, 25);
+
+            apinitialX = aprandom.Next((int)apSize + apminEdgeDistance, appictureBox.Width - (int)apSize - apminEdgeDistance);
+            apinitialY = aprandom.Next((int)apSize + apminEdgeDistance, appictureBox.Height - (int)apSize - apminEdgeDistance);
+
+            do
+            {
+                apinitialVelocityX = (float)(aprandom.NextDouble() * 6 - 2);
+            } while (Math.Abs(apinitialVelocityX) < 0.5);
+
+            do
+            {
+                apinitialVelocityY = (float)(aprandom.NextDouble() * 6 - 2);
+            } while (Math.Abs(apinitialVelocityY) < 0.5);
+
+            apX = apinitialX;
+            apY = apinitialY;
+            apVelocityX = apinitialVelocityX;
+            apVelocityY = apinitialVelocityY;
+
+            apColor = Color.FromArgb(aprandom.Next(256), aprandom.Next(256), aprandom.Next(256));
+        }
+
+        public void Move()
+        {
+            apX += apVelocityX;
+            apY += apVelocityY;
+
+            if (apX <= 0 || apX >= appictureBox.Width - apSize) apVelocityX = -apVelocityX;
+            if (apY <= 0 || apY >= appictureBox.Height - apSize) apVelocityY = -apVelocityY;
+
+            aptrail.Insert(0, new PointF(apX, apY));
+            if (aptrail.Count > apTrailLength) aptrail.RemoveAt(aptrail.Count - 1);
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Brush apbrush = new SolidBrush(apColor))
+            {
+                g.FillEllipse(apbrush, apX, apY, apSize, apSize);
+            }
+
+            for (int i = 0; i < aptrail.Count; i++)
+            {
+                float apalpha = 1 - (float)i / apTrailLength;
+                using (Brush apbrush = new SolidBrush(Color.FromArgb((int)(apalpha * 255), apColor)))
+                {
+                    float apsize = apSize * apalpha;
+                    g.FillEllipse(apbrush, aptrail[i].X + (apSize - apsize) / 2, aptrail[i].Y + (apSize - apsize) / 2, apsize, apsize);
+                }
+            }
+        }
+        public void Reset()
+        {
+            apX = apinitialX;
+            apY = apinitialY;
+            apVelocityX = apinitialVelocityX;
+            apVelocityY = apinitialVelocityY;
+        }
+        public void RandomizePosition()
+        {
+            int apminEdgeDistance = 10;
+            apX = aprandom.Next((int)apSize + apminEdgeDistance, appictureBox.Width - (int)apSize - apminEdgeDistance);
+            apY = aprandom.Next((int)apSize + apminEdgeDistance, appictureBox.Height - (int)apSize - apminEdgeDistance);
+
+            apColor = Color.FromArgb(aprandom.Next(256), aprandom.Next(256), aprandom.Next(256));
+        }
+        public void GenerateExplosion(Random random)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                explosionParticles.Add(new Particle(this.apX + this.apSize / 2, this.apY + this.apSize / 2, random));
+            }
+        }
+        public void UpdateParticles()
+        {
+            explosionParticles.ForEach(p => p.Update());
+            explosionParticles = explosionParticles.Where(p => p.IsAlive()).ToList();
+        }
+
+        public void DrawParticles(Graphics g)
+        {
+            explosionParticles.ForEach(p => p.Draw(g));
+        }
+    }
+    public class Particle
+    {
+        public float X, Y, VelocityX, VelocityY, Size;
+        public Color Color;
+        private float lifespan = 1.0f;
+
+        public Particle(float x, float y, Random random)
+        {
+            X = x;
+            Y = y;
+            VelocityX = (float)(random.NextDouble() * 4 - 2);
+            VelocityY = (float)(random.NextDouble() * 4 - 2);
+            Size = random.Next(2, 5);
+            Color = Color.FromArgb(255, random.Next(200, 256), random.Next(100, 256), random.Next(50, 200));
+        }
+
+        public bool IsAlive() => lifespan > 0;
+
+        public void Update()
+        {
+            X += VelocityX;
+            Y += VelocityY;
+            lifespan -= 0.05f;
+        }
+
+        public void Draw(Graphics g)
+        {
+            if (lifespan <= 0) return;
+            using (Brush brush = new SolidBrush(Color.FromArgb((int)(lifespan * 255), Color)))
+            {
+                g.FillEllipse(brush, X - Size / 2, Y - Size / 2, Size, Size);
+            }
+        }
+    }
+}
